8030835ecae34b97d4fbdb5755d1830cc20650b3
Kazuhiko UNO <kazuhiko.uno@gmail.com>
2014-03-14 17:27:17 +0900

    add tests

--- tests/EmptyTest.php
+++ tests/EmptyTest.php
@@ -31,9 +31,33 @@ class EmptyTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(empty($value));
     }
     
-    function testFalseという文字列はEmptyじゃない()
+    function testFalseという文字列はEmptyではない()
     {
         $value = 'False';
         $this->assertFalse(empty($value));
+    }
+    
+    function test存在しない添字の配列に使えるかつEmptyです()
+    {
+        $value = array();
+        $this->assertTrue(empty($value['uso800']));
+    }
+    
+    function test連想配列で値がnullの場合はEmptyです()
+    {
+        $value = array('uso800' => null);
+        $this->assertTrue(empty($value['uso800']));
+    }
+    
+    function test連想配列で値がfalseの場合はEmptyです()
+    {
+        $value = array('uso800' => false);
+        $this->assertTrue(empty($value['uso800']));
+    }
+    
+    function test連想配列で値がtrueの場合はEmptyではない()
+    {
+        $value = array('uso800' => true);
+        $this->assertFalse(empty($value['uso800']));
     }
 }
